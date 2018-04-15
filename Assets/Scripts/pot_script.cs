21a3864112e93cb15be42e8e4cfb64fd95467d63
Bryce Collins <brycecollins0@gmail.com>
2018-04-15 15:53:27 -0700

    POT IS GOOD

--- Assets/Scripts/pot_script.cs
+++ Assets/Scripts/pot_script.cs
@@ -7,7 +7,11 @@ public class pot_script : MonoBehaviour {
 	public GameObject temp_object;
 	public GameObject onion_actual;
 
+	public Sprite onion_sprite;
+
 	public Vector2 spawn_range;
+
+	public float multiplier;
 
 	private float time_of_next_spawn;
 	// Use this for initialization
@@ -19,10 +23,15 @@ public class pot_script : MonoBehaviour {
 	void Update () {
 		if(time_of_next_spawn <= Time.time){
 			spawn();
+			time_of_next_spawn = Random.Range(spawn_range.x, spawn_range.y) + Time.time;
 		}
 	}
 
 	public void spawn(){
-
+		GameObject temp = Instantiate(temp_object, transform.position + new Vector3(0f, 0.8f, 0f), Quaternion.identity);
+		Rigidbody2D temp_rigidbody = temp.GetComponent<Rigidbody2D>();
+		temp_spawn spawn = temp.GetComponent<temp_spawn>();
+		spawn.setup(onion_actual, onion_sprite);
+		temp_rigidbody.velocity = Random.insideUnitCircle * multiplier + new Vector2(0f, 1.5f);
 	}
 }
